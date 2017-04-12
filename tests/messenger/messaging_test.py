2e2cca2ecfcda35612c7d45f1370ddb3f45d2dbf
Paul Vergeev <paulvergeev@gmail.com>
2017-04-12 21:59:17 +0300

    Add vcr cassettes for send_main_menu

--- tests/messenger/messaging_test.py
+++ tests/messenger/messaging_test.py
@@ -1,18 +1,20 @@
 import os
+import unittest
 
-import vcr_unittest
+import vcr
 
 from app.messenger import messaging
 
 
-class MessagingTestCase(vcr_unittest.VCRTestCase):
+class MessagingTestCase(unittest.TestCase):
     
     def setUp(self):
         self.access_token = os.environ['ACCESS_TOKEN']
         self.user_id = '474276666029691'
 
     def test_send_main_menu(self):
-        response = messaging.send_main_menu(self.access_token, self.user_id)
+        with vcr.use_cassette('vcr_cassettes/send_main_menu.yaml'):
+            response = messaging.send_main_menu(self.access_token, self.user_id)
         self.assertTrue('recipient_id' in response)
         self.assertTrue('message_id' in response)
         self.assertEqual(response['recipient_id'], self.user_id)
