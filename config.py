69f7490b6ed28c28784148295dec2144344f4ed8
Paul Vergeev <paulvergeev@gmail.com>
2017-04-19 15:36:14 +0300

    Remove automatic fallback to SQLite
    It's better to be explicit if there's no DATABASE_URL.

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 import os
 
-
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///meetup.db'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False  # supress deprecation warning
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_TRACK_MODIFICATIONS = False  # suppress deprecation warning
 
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 PAGE_ID = os.environ['PAGE_ID']
