c713273fe145418113d750579f8b135dc513c3b8
voron434 <070809010@mail.ru>
2017-04-20 12:34:27 +0300

    Delete default case for SQLALCHEMY_DATABASE_URI
    if user doesn't set it, he coud have some problems with SQLite

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 
 
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///meetup.db'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # supress deprecation warning
