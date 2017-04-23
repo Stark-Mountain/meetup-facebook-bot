f5a827e0b4c7635a85b65462c2850a9afdc69d43
voron434 <070809010@mail.ru>
2017-04-23 15:11:48 +0300

    Add set_like test

--- tests/models/talk_test.py
+++ tests/models/talk_test.py
@@ -1,14 +1,26 @@
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
-from meetup_facebook_bot.models import talk
+from meetup_facebook_bot.models.talk import Talk
 
 class TalkTestCase(TestCase):
 
+    def setUp(self):
+        self.db_session = MagicMock()
+        self.user_id = 1
+        self.talk_id = 1
+    
     def test_is_liked_by(self):
         pass
-
-    def test_set_like(self):
-        pass
+    
+    @patch('meetup_facebook_bot.models.talk.Like')
+    def test_set_like(self, like_class_mock):
+        real_talk = Talk(id=self.talk_id)
+        mock_like = MagicMock()
+        like_class_mock.return_value = mock_like
+        real_talk.is_liked_by = MagicMock(return_value=False)
+        real_talk.set_like(self.user_id, self.db_session)
+        like_class_mock.assert_called_once_with(user_facebook_id=self.user_id, talk_id=self.talk_id)
+        self.db_session.add.assert_called_once_with(mock_like)
 
     def test_unset_like(self):
         pass
