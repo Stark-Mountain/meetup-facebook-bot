92e323b2f177190097e50b788a92f4b349fe760f
Ilya Osipov <070809010@mail.ru>
2017-04-23 03:51:27 +0300

    Add basic architecture

---
+++ tests/models/talk_test.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from meetup_facebook_bot.models import talk
+
+class TalkTestCase(TestCase):
+
+    def test_is_liked_by(self):
+        pass
+
+    def test_set_like(self):
+        pass
+
+    def test_unset_like(self):
+        pass
+
+    def test_revert_like(self):
+        pass
