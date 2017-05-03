b9374c5adbae45c8172d20e2e99ac37bdc7de261
Paul Vergeev <paulvergeev@gmail.com>
2017-05-03 20:15:14 +0300

    Add code for avoiding duplicating tables

--- database_setup.py
+++ database_setup.py
@@ -1,8 +1,13 @@
 import json
+import sys
 
 from meetup_facebook_bot import server
 from meetup_facebook_bot.models import base, talk, speaker
 
+
+if server.engine.dialect.has_table(server.engine.connect(), "talks"):
+    print('Table talks exists, won\'t run create_all()')
+    sys.exit()
 
 base.Base.metadata.create_all(bind=server.engine)
 session = server.Session()
