93a2a99597c68d1fa7fd86fa851214db310234a5
mcproger <artem7lomonosov@gmail.com>
2017-05-05 21:06:41 +0300

    Rename db_manage

--- db_manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-
-if __name__ == '__main__':
-    main(repository='database_migrate', debug='False', url=os.environ['DATABASE_URL']) 
