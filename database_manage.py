deb8be596af05c28664f084a1e66f3cdbc5894aa
mcproger <artem7lomonosov@gmail.com>
2017-05-05 21:42:09 +0300

    Fix import os module

--- database_manage.py
+++ database_manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from migrate.versioning.shell import main
 import os
+from migrate.versioning.shell import main
 
 if __name__ == '__main__':
     main(repository='database_migrate', debug='False', url=os.environ['DATABASE_URL']) 
