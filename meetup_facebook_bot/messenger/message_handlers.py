f125b48dcd33d85ebc9368e5c249b9cd6c0f99d0
Ilya Osipov <070809010@mail.ru>
2017-05-08 00:03:36 +0300

    Add 'if' block, because this handler skip quick reply

--- meetup_facebook_bot/messenger/message_handlers.py
+++ meetup_facebook_bot/messenger/message_handlers.py
@@ -1,5 +1,6 @@
 from meetup_facebook_bot.models.talk import Talk
 from meetup_facebook_bot.messenger import messaging
+from meetup_facebook_bot.messenger.message_validators import is_talk_rate_command
 
 
 def handle_talk_info_command(messaging_event, access_token, db_session):
@@ -32,4 +33,6 @@ def handle_talk_like_command(messaging_event, access_token, db_session):
 def handle_message_with_sender_id(messaging_event, access_token, db_session):
     sender_id = messaging_event['sender']['id']
     talks = db_session.query(Talk).all()
+    if is_talk_rate_command(messaging_event):
+        return
     return messaging.send_schedule(access_token, sender_id, talks, db_session)
