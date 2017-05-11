a70b74478f3ae414187f0bc2a1f34d5cde394ae7
Paul Vergeev <paulvergeev@gmail.com>
2017-05-11 20:37:24 +0300

    Revert "Revert "Close #48""
    This reverts commit 815513f8f649b81567ab871e3975ffa8883270d9.

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite://')
 
+
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 PAGE_ID = os.environ.get('PAGE_ID')
 APP_ID = os.environ.get('APP_ID')
 VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
