a43a4610194a5ba21c1dd33801790b6f62e1b158
Paul Vergeev <paulvergeev@gmail.com>
2017-05-12 22:13:53 +0300

    Remove fab commit
    The goals are better achieved with CI.

--- fabfile.py
+++ fabfile.py
@@ -267,26 +267,3 @@ def print_service_status(service_name):
 def status():
     env.sudo_password = getpass('Initial value for env.sudo_password: ')
     print_service_status(UWSGI_SERVICE_NAME)
-
-
-def test():
-    with settings(warn_only=True):
-        result = local('python3 -m pytest tests', capture=True)
-    if result.failed and not confirm('Tests failed. Continue anyway?'):
-        abort('Aborting at user request.')
-
-
-def git_commit():
-    with settings(warn_only=True):
-        local('git add -i && git commit')
-
-
-def push(branch):
-    local('git push origin %s' % branch)
-
-
-@task
-def commit(branch):
-    test()
-    git_commit()
-    push(branch)
