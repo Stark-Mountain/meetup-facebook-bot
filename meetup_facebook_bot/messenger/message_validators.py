9fc4ab15440c7cdce3f68124fd9cc97d0e1a80d6
Paul Vergeev <paulvergeev@gmail.com>
2017-05-13 20:24:40 +0300

    Add links to docs

--- meetup_facebook_bot/messenger/message_validators.py
+++ meetup_facebook_bot/messenger/message_validators.py
@@ -1,4 +1,7 @@
 def is_quick_button(messaging_event):
+    """ More about Quick Replies at:
+        https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
+    """
     if 'message' not in messaging_event:
         return False
     if 'quick_reply' not in messaging_event['message']:
@@ -7,7 +10,21 @@ def is_quick_button(messaging_event):
 
 
 def is_postback_button(messaging_event):
+    """ More about Postback Buttons at:
+        https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback
+    """
     return 'postback' in messaging_event
+
+
+def is_message_with_text(messaging_event):
+    """ More about simple text messages at:
+        https://developers.facebook.com/docs/messenger-platform/webhook-reference/message
+    """
+    if 'message' not in messaging_event:
+        return False
+    if 'text' not in messaging_event['message']:
+        return False
+    return True
 
 
 def is_talk_ask_command(messaging_event):
@@ -38,14 +55,6 @@ def is_no_ask_question_url_postback(messaging_event):
     if not is_postback_button(messaging_event):
         return False
     return 'ask question no url' in messaging_event['postback']['payload']
-
-
-def is_message_with_text(messaging_event):
-    if 'message' not in messaging_event:
-        return False
-    if 'text' not in messaging_event['message']:
-        return False
-    return True
 
 
 def is_appropriate_to_send_schedule(messaging_event):
