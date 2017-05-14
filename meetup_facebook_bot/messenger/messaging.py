ce15800b6bf8133c11b2533372828c591bf6435d
Paul Vergeev <paulvergeev@gmail.com>
2017-05-14 12:46:02 +0300

    Remove db_session from messaging completely
    Delegating database manipulations to handlers simplifies messaging
    module without complicating handlers' code.

--- meetup_facebook_bot/messenger/messaging.py
+++ meetup_facebook_bot/messenger/messaging.py
@@ -2,11 +2,8 @@
 from meetup_facebook_bot.messenger import facebook_api
 
 
-def send_rate_menu(access_token, user_id, talk, db_session):
-    if talk.is_liked_by(user_id, db_session):
-        rate_button_title = 'Убрать лайк'
-    else:
-        rate_button_title = 'Поставить лайк'
+def send_rate_menu(access_token, user_id, talk, talk_liked):
+    rate_button_title = 'Убрать лайк' if talk_liked else 'Поставить лайк'
     rate_button_payload = 'like talk %d' % talk.id
     cancel_button_title = 'Отменить'
     cancel_button_payload = 'cancel payload'
@@ -23,12 +20,10 @@ def send_rate_menu(access_token, user_id, talk, db_session):
     return facebook_api.send_quick_replies(access_token, user_id, text, buttons)
 
 
-def send_like_confirmation(access_token, user_id, talk, db_session):
-    talk_title = talk.title
-    if talk.is_liked_by(user_id, db_session):
-        like_text_message = 'Поставил лайк докладу:\n %s' % talk_title
-    else:
-        like_text_message = 'Убрал лайк c доклада:\n %s' % talk_title
+def send_like_confirmation(access_token, user_id, talk, talk_liked):
+    like_confirmation = 'Поставил лайк докладу %s' % talk.title
+    unlike_confirmation = 'Убрал лайк c доклада %s' % talk.title
+    like_text_message = like_confirmation if talk_liked else unlike_confirmation
     return facebook_api.send_text_message(access_token, user_id, like_text_message)
 
 
@@ -48,30 +43,33 @@ def create_schedule_page_subtitle(like_text, number_of_likes, speaker_name):
     )
 
 
-def send_schedule(access_token, user_id, talks, db_session):
+def create_schedule_page(talk, number_of_likes, talk_liked):
+    like_text = 'Вы лайкнули этот доклад' if talk_liked else 'Вы не оценили этот докад'
+    page_subtitle = create_schedule_page_subtitle(
+        like_text,
+        number_of_likes,
+        talk.speaker.name
+    )
+    more_talk_info_button = facebook_api.create_postback_button(
+        title='Получить подробности',
+        payload='info talk %d' % talk.id
+    )
+    rate_button = facebook_api.create_postback_button(
+        title='Оценить',
+        payload='rate talk %d' % talk.id
+    )
+    ask_question_button = create_ask_question_button(talk.ask_question_url)
+    buttons = [more_talk_info_button, rate_button, ask_question_button]
+    page = facebook_api.create_generic_template_page(talk.title, page_subtitle, buttons)
+    return page
+
+
+def send_schedule(access_token, user_id, talks, talk_like_numbers, liked_talk_ids):
     generic_template_pages = []
-    for talk in talks:
-        number_of_likes = talk.count_likes(db_session)
-        if talk.is_liked_by(user_id, db_session):
-            like_text = 'Вы лайкнули этот доклад'
-        else:
-            like_text = 'Вы не оценили этот докад'
-        page_subtitle = create_schedule_page_subtitle(
-            like_text,
-            number_of_likes,
-            talk.speaker.name
-        )
-        more_talk_info_button = facebook_api.create_postback_button(
-            title='Получить подробности',
-            payload='info talk %d' % talk.id
-        )
-        rate_button = facebook_api.create_postback_button(
-            title='Оценить',
-            payload='rate talk %d' % talk.id
-        )
-        ask_question_button = create_ask_question_button(talk.ask_question_url)
-        buttons = [more_talk_info_button, rate_button, ask_question_button]
-        page = facebook_api.create_generic_template_page(talk.title, page_subtitle, buttons)
+    for talk_index, talk in enumerate(talks):
+        number_of_likes = talk_like_numbers[talk.id]
+        talk_liked = talk.id in liked_talk_ids
+        page = create_schedule_page(talk, number_of_likes, talk_liked)
         generic_template_pages.append(page)
     return facebook_api.send_generic_template(access_token, user_id, generic_template_pages)
 
