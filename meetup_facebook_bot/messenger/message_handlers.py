52762c839209a6b49483a83d9e994e9c6f661092
Ilya Osipov <070809010@mail.ru>
2017-05-16 23:44:15 +0300

    Add error logging in handlers

--- meetup_facebook_bot/messenger/message_handlers.py
+++ meetup_facebook_bot/messenger/message_handlers.py
@@ -1,3 +1,5 @@
+import logging
+
 from meetup_facebook_bot.models.talk import Talk
 from meetup_facebook_bot.models.speaker import Speaker
 from meetup_facebook_bot.models.like import Like
@@ -10,6 +12,7 @@ def handle_talk_info_command(messaging_event, access_token, db_session):
     talk_id = int(payload.split(' ')[-1])
     talk = db_session.query(Talk).get(talk_id)
     if not talk:
+        logging.error('No talk founded with this id: %s' % talk_id)
         return
     return messaging.send_talk_info(access_token, sender_id, talk)
 
@@ -19,6 +22,9 @@ def handle_talk_rate_command(messaging_event, access_token, db_session):
     payload = messaging_event['postback']['payload']
     talk_id = int(payload.split(' ')[-1])
     talk = db_session.query(Talk).get(talk_id)
+    if not talk:
+        logging.error('No talk founded with this id: %s' % talk_id)
+        return
     talk_liked = talk.is_liked_by(sender_id, db_session)
     return messaging.send_rate_menu(access_token, sender_id, talk, talk_liked)
 
@@ -28,6 +34,9 @@ def handle_talk_like_command(messaging_event, access_token, db_session):
     payload = messaging_event['message']['quick_reply']['payload']
     talk_id = int(payload.split(' ')[-1])
     talk = db_session.query(Talk).get(talk_id)
+    if not talk:
+        logging.error('No talk founded with this id: %s' % talk_id)
+        return
     talk.revert_like(sender_id, db_session)
     talk_liked = talk.is_liked_by(sender_id, db_session)
     return messaging.send_like_confirmation(access_token, sender_id, talk, talk_liked)
