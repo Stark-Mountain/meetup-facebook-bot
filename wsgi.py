b409c89dcbe5529819111630a2fbc4b4b6299416
Ilya Osipov <070809010@mail.ru>
2017-05-17 00:03:42 +0300

    Change path to log

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ import logging
 from meetup_facebook_bot.server import app
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='/example.log', level=logging.DEBUG)
+    logging.basicConfig(filename='~/example.log', level=logging.DEBUG)
     app.run()
