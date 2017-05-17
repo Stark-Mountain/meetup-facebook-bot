239f05d93b72c9ead454dff2e1d3247803b3b65c
Ilya Osipov <070809010@mail.ru>
2017-05-17 17:11:01 +0300

    Test

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,5 @@ from meetup_facebook_bot.server import app
 from fabfile import LOG_PATH
 
 if __name__ == '__main__':
-    logging.basicConfig(filename=LOG_PATH+'/voron434.log', level=logging.DEBUG)
     logging.debug('Start new build')
     app.run()
