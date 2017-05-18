a00db471be15690847b68271d36ce4b399ca72c4
Ilya Osipov <070809010@mail.ru>
2017-05-18 12:11:19 +0300

    Make logging baseconfig global

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,10 @@ import logging
 
 from meetup_facebook_bot.server import app
 
+
+logging.basicConfig(filename='/var/log/meetup-facebook-bot/voron434.log', level=logging.DEBUG)
+
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='/var/log/meetup-facebook-bot/voron434.log', level=logging.DEBUG)
     logging.debug('Start new build')
     app.run()
