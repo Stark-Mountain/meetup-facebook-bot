fe640619c78e6251d54153d0238a44352e5815d5
Ilya Osipov <070809010@mail.ru>
2017-05-19 00:10:23 +0300

    Add rotation example from documentation

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
 import logging
+import logging.handlers
 
 from meetup_facebook_bot.server import app
 
 
-logging.basicConfig(filename='/var/log/meetup-facebook-bot/voron434.log', level=logging.DEBUG)
+LOG_FILENAME = '/var/log/meetup-facebook-bot/voron434.log'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+main_logger = logging.getLogger('MainLogger')
+main_logger.setLevel(logging.DEBUG)
+rotation_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=20, backupCount=5)
+main_logger.addHandler(rotation_handler)
 
 
 if __name__ == '__main__':
