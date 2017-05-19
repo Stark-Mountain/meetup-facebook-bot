c9c1e2d172ed664e3124670f1aaed1b426c8dbe7
Ilya Osipov <070809010@mail.ru>
2017-05-19 21:30:44 +0300

    Make it so that logs not break

--- log_config.py
+++ log_config.py
@@ -1,15 +1,1 @@
-import logging
 
-from logging.handlers import TimedRotatingFileHandler
-
-
-def run_logging():
-    log_filename = '/var/log/meetup-facebook-bot/main_logs.log'
-    logger = logging.getLogger("run_logging")
-    logger.setLevel(logging.DEBUG)
-    rotation_handler = TimedRotatingFileHandler(log_filename,
-                                       when="m",
-                                       interval=1,
-                                       backupCount=5)
-    logger.addHandler(rotation_handler)
-    logger.info('Start new build')
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,18 @@
-from log_config import run_logging
+import logging
+
+from logging.handlers import TimedRotatingFileHandler
+
 from meetup_facebook_bot.server import app
 
 
 if __name__ == '__main__':
-    run_logging()
+    log_filename = '/var/log/meetup-facebook-bot/main_logs.log'
+    logger = logging.getLogger("run_logging")
+    logger.setLevel(logging.DEBUG)
+    rotation_handler = TimedRotatingFileHandler(log_filename,
+                                                when="m",
+                                                interval=1,
+                                                backupCount=5)
+    logger.addHandler(rotation_handler)
+    logger.info('Start new build')
     app.run()
