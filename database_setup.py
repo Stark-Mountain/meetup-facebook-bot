b4aa5beeda5f7602a63d8f16d041774a6c353cec
voron434 <070809010@mail.ru>
2017-05-24 11:53:33 +0300

    Delete unused imports

--- database_setup.py
+++ database_setup.py
@@ -1,8 +1,6 @@
-import json
 import sys
 
 from meetup_facebook_bot import server
-from meetup_facebook_bot.models import base, talk, speaker
 
 
 if server.engine.dialect.has_table(server.engine.connect(), "talks"):
